8afc9189ec4edbec394453949ee06ccc4baf2d3f
Ilian Cholpanov <iliancholpanov@gmail.com>
2023-11-09 08:52:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -73,6 +73,14 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  // else alert("Sorry we're closed");
+
   return (
     <footer>{new Date().toLocaleTimeString()}. We're currently open</footer>
   );
